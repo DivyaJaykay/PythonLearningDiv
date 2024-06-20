6ee06e60b269cf79ce79980d3394275ae11c95a9
DivyaJaykay <divyajayakumar99@gmail.com>
2024-06-20 13:37:05 +0530

    Add files via upload

---
+++ lab004.py
@@ -0,0 +1,18 @@
+name="divya"
+print(name)
+name2= "Sandy"
+print(name2)
+
+x=5
+print(type(x))
+
+print(type(name))
+isFemale=True
+print(type(isFemale))
+
+pi=3.14
+print(type(pi))
+
+cn=5+5j
+print(cn)
+print(type(cn))
