40c7aa7009ab0828325d6c5a5943add2eb9e59af
DivyaJaykay <divyajayakumar99@gmail.com>
2024-07-04 13:11:55 +0530

    Add files via upload

---
+++ lab016.py
@@ -0,0 +1,13 @@
+name="this is various functions of string program"
+
+print(type(name))
+
+print(len(name))
+
+print(name[0:6])
+print(name[2:12])
+print(r'C:\some\name')
+print(name[6])
+
+print(list(name))
+print(slice(name))
