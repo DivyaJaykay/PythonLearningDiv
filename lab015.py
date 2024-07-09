31041ef7cc7c0c8efa7ccc7b24df7b4305f9b89b
DivyaJaykay <divyajayakumar99@gmail.com>
2024-07-09 10:54:39 +0530

    lab015.py

--- lab015.py
+++ lab015.py
@@ -1,6 +1,25 @@
 num1=int(input("Enter your number1:"))
-n=2
-multiply=num1*n
-print(f"The result of multiplying is \n {num1}*{n}={multiply}")
+n=1
+def mul(n):
+    multiply = num1 * n
+    print(f'{num1}*{n}={multiply}')
+mul(n);
+mul(n+1)
+mul(n+2)
+mul(n+3)
+mul(n+4)
+mul(n+5)
+mul(n+6)
+mul(n+7)
+mul(n+8)
+mul(n+9)
+
+
+
+
+
+
+
+
 
 
