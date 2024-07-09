3c57a49902cb27d842a62788942d90b6352c2de4
DivyaJaykay <divyajayakumar99@gmail.com>
2024-07-09 11:24:27 +0530

    Add files via upload

---
+++ lab018.py
@@ -0,0 +1,11 @@
+#Arithmetic operators
+x=int(input("Enter the value of x\n"))
+y=int(input("Enter the value of y\n"))
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y) #5times of 20
+print(x%y) #brings remainder
+print(x//y) #brings quotient
