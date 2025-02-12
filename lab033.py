b7766d52812c0ff7cdbf6b37138833f312d0abfd
DivyaJaykay <divyajayakumar99@gmail.com>
2025-02-12 12:45:46 +0530

    Add files via upload

---
+++ lab033.py
@@ -0,0 +1,12 @@
+#Calculating the square and cube for given numbers
+
+num1 = int(input("Enter the number1"))
+num2 = int(input("Enter the number2"))
+square= num1**2
+print("The value of num1 is:",square, end='\n')
+cube=num2**3
+print("The value of num2 is:",cube, end='\n')
+
+
+
+
