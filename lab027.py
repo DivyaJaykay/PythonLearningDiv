251d8a48d4aa1fe62857a9b71a69b9d22b92daa2
DivyaJaykay <divyajayakumar99@gmail.com>
2025-02-12 12:35:49 +0530

    Add files via upload

---
+++ lab027.py
@@ -0,0 +1,33 @@
+
+
+# Grade calculator program
+grade=63
+
+
+if grade >=90 and grade <=100:
+   {
+Print('Your score is A')
+   }
+elif grade>=80 and grade<=89:
+   {
+   print('Your score is B')
+   }
+if grade>=70 and grade<=79:
+   {
+      print ('Your score is C')
+   }
+elif grade>=60 and grade<=69:
+   {
+      print('Your score is D')
+   }
+if grade>=0 and grade<=59:
+   {
+      print("Your score is F")
+   }
+
+
+
+
+
+
+
